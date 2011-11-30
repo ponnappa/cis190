d2c681d3210ed1d0d49e46b9f93c1a684b66b139
Austin Kasserman <austinkass@gmail.com>
2011-11-29 21:44:38 -0500

    Room.h

---
+++ Room.h
@@ -0,0 +1,37 @@
+#ifndef __ROOM_HEADER__
+#define __ROOM_HEADER__
+
+#include <string>
+#include <vector>
+
+class Room {
+public:
+    Room(string _name);
+    
+    string getName();
+    
+    vector<Room> getExits();
+    void addExit(Room r);
+    bool removeExit(Room r);
+    
+    vector<Npc> getNpcs();
+    void addNpc(Npc n);
+    bool removeNpc(Npc n);
+    
+    vector<Item> getItems();
+    void addItem(Item i);
+    bool removeItem(Item i);
+    
+    vector<Observation> getObservations();
+    void addObservation(Observation o);
+    bool removeObservation(Observation o);
+
+private:
+    string name;
+    vector<Room> exits;
+    vector<Npc> npcs;
+    vector<Item> items;
+    vector<Observation> observations;  
+};
+
+#endif
