32a5f4168fb839b33ccb4b3eaed32728ee19100b
ponnappa <nbponnappa@gmail.com>
2011-12-04 17:53:46 -0500

    Update GameEngine.cpp

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -2,33 +2,72 @@
 #include <vector>
 #include <string>
 #include <iostream>
-using namespace std;
+#include <stdlib.h>
+#include "Player.cpp"
 
+using namespace std;
 
 int getUserInput(int n){
 
 	string in;
 	while(true){
-        	cout<<"Please enter a selection from 1 to " <<n<<endl;
-        	cin>>in;
-        	if (in.compare("q")==0) exit(0);
-        	for(int i=1;i<=n;i++){
-	            if (atoi(in.c_str())==i) return atoi(in.c_str());
-	        }
-    }
-    
+		cout<<"Please enter a selection from 1 to " <<n<<endl;
+		cin>>in;
+		if (in.compare("back")==0) return -1;
+		if(in.compare("q")==0) exit(0);
+		for(int i=1;i<=n;i++){
+			if (atoi(in.c_str())==i) return atoi(in.c_str());
+		}
+	}
 }
 
 
-
 int main(){
-
+	string s;
 	Room *r = new Room("Bedroom");
+	Room *at = new Room("Attic");
+	Room *ha = new Room("Hall");
+	(*r).addExit(at);
+	(*at).addExit(r);//fix addExit to duplicate
+	(*r).addExit(ha);
+	(*ha).addExit(r);
 	Player *p = new Player("John",r);
 	cout<<"player "<<(*p).getName()<<" in room "<<(*(*p).getCurrentRoom()).getName()<<endl;
-	int in = getUserInput(5);
-    
-	cout<<in<<endl;
+	
+	while(true){
+		cout<<"1)Move"<<endl;
+		cout<<"2)Observe room"<<endl;
+		cout<<"3)Talk to people"<<endl;
+		cout<<"4)Look at journal"<<endl;
+		int input= getUserInput(4);
+		if(input==1){
+			vector<Room*> ex = (*(*p).getCurrentRoom()).getExits();
+			for(unsigned int i=0; i<ex.size();i++){
+				cout<<(i+1)<<")"<<(*(ex.at(i))).getName()<<endl;
+			}
+			cout<<"type \"back\" to go back"<<endl<<endl;
+			int movein = getUserInput(ex.size());
+			if(movein==-1)
+				continue;
+			(*p).setCurrentRoom(ex.at(movein-1));
+			continue;
+		}
+		if(input==2){
+			continue;
+		}
+		if(input==3){
+			continue;
+		}
+		if(input==4){
+			cout<<"Player "<<(*p).getName()<<" is in room "<<(*(*p).getCurrentRoom()).getName()<<endl<<endl;
+			continue;
+		}
+	//	string s2;
+	//	cin>>s;
+		//cout<<"test "<<s << endl;
+	}
+	
+
 
 	return 0;
 }
