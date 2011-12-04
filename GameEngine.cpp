bd3dd58e44d6ada952012260a4d23a51aa28d9f8
ponnappa <nbponnappa@gmail.com>
2011-12-04 16:48:39 -0500

    Update GameEngine.cpp

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -7,15 +7,14 @@ using namespace std;
 
 int getUserInput(int n){
 
-    string in;
-    
-    while(true){
-        cout<<"Please enter a selection from 1 to " <<n<<endl;
-        cin>>in;
-        if (in.compare("q")==0) exit(0);
-        for(int i=1;i<=n;i++){
-            if (atoi(in.c_str())==i) return atoi(in.c_str());
-        }
+	string in;
+	while(true){
+        	cout<<"Please enter a selection from 1 to " <<n<<endl;
+        	cin>>in;
+        	if (in.compare("q")==0) exit(0);
+        	for(int i=1;i<=n;i++){
+	            if (atoi(in.c_str())==i) return atoi(in.c_str());
+	        }
     }
     
 }
@@ -23,10 +22,13 @@ int getUserInput(int n){
 
 
 int main(){
-	
-    int in = getUserInput(5);
+
+	Room *r = new Room("Bedroom");
+	Player *p = new Player("John",r);
+	cout<<"player "<<(*p).getName()<<" in room "<<(*(*p).getCurrentRoom()).getName()<<endl;
+	int in = getUserInput(5);
     
-    cout<<in<<endl;
+	cout<<in<<endl;
 
 	return 0;
 }
