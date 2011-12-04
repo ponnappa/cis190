48d5f6b082398a9ed18380138772054d799993d4
Austin Kasserman <austinkass@gmail.com>
2011-12-04 16:44:18 -0500

    Austin-user input function in GameEngine

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -4,14 +4,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	string s;
-	while(s.compare("q")!=0){
-		cin>>s;
-		cout<<"test "<<s << endl;
-	}
-	
 
+int getUserInput(int n){
+
+    string in;
+    
+    while(true){
+        cout<<"Please enter a selection from 1 to " <<n<<endl;
+        cin>>in;
+        if (in.compare("q")==0) exit(0);
+        for(int i=1;i<=n;i++){
+            if (atoi(in.c_str())==i) return atoi(in.c_str());
+        }
+    }
+    
+}
+
+
+
+int main(){
+	
+    int in = getUserInput(5);
+    
+    cout<<in<<endl;
 
 	return 0;
 }
