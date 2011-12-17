c95bfcc36a78513d126859b76a0aa72207809ae0
ponnappa <nbponnappa@gmail.com>
2011-12-17 17:11:00 -0500

    Update Observation.h

--- Observation.h
+++ Observation.h
@@ -11,13 +11,23 @@ private:
 	string name;
 	string description;
 	Item *item;
+	Item *take;
+	set<int> triggers;
+	map<int,bool> changes;
+	Game *game;
 public:
-	Observation(string _name, string _description);
+	Observation(string _name, string _description, Game *_game);
 	bool equals(Observation *o);
 	string getName();
 	string getDescription();
 	void addItem(Item *i);
 	bool hasItem();
+	Item* removeItem();
+	bool hasTake();
+	Item* removeTake();
+	void addTrigger(int i);
+	set<int> getTriggers();
+	void addChange(int i, bool b);
+	map<int,bool> getChanges();
 };
 #endif // OBSERVATION_H
-
